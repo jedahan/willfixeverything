bad54e63a3fae239d699b16df8f309a6f41c06a9
Jonathan Dahan <git@jonathan.is>
2021-05-25 09:50:42 -0700

    sinatra to static + javascript

--- hello.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-require 'erb'
-
-get '/' do
-  @thing = @request.env['SERVER_NAME'].split('.willfixeverything').first.split('.').join(" ").to_s
-  erb :index
-end
